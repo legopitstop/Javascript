0f5f5381b7a0f2e34990a5192957ab8ef26015c4
Legopitstop <1589lego@gmail.com>
2021-07-27 13:43:19 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,277 @@
+# Javascript Functions
+This repo contains a few useful functions, below you can find how to install and the features of each function.
+
+# LPSCommon
+This contains many small, but useful functions. Note that this function does use some Jquery functions
+## Install
+Install [LPSCommon.js]()
+```html
+<!--Jquery-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src=""></script>
+```
+
+## Functions
+### isRequired();
+Any element with the `required` property will use this function, If the input is empty it will make the border red.
+#### Syntax
+```ts
+isRequired(): void;
+```
+
+### rng();
+Returns a random number using the minium and maxium numbers given.
+#### Syntax
+- min The minium number to except.
+- max The maxium number to except.
+```ts
+rng(min: number, max: number): void;
+```
+
+### clearElement();
+Clears all contents of the element.
+#### Syntax
+- confirm0 When true it will ask the user if they want to clear it.
+- element The querySelector to use.
+```ts
+clearElement(confirm0: boolean, element: string): void;
+```
+
+### copyElement();
+Copy the contents of the element to the users clipboard.
+#### Syntax
+- element The querySelector to use.
+```ts
+copyElement(element: string): void;
+```
+
+### cutElement();
+Cut the contents of the element.
+#### Syntax
+- element The querySelector to use.
+```ts
+cutElement(element: string): void;
+```
+
+### pressKey();
+Returns the button that the user pressed.
+#### Syntax
+- event The element to check
+```ts
+pressKey(event: string): void;
+```
+
+### parseURLParams();
+Returns the form output from the URL.
+#### Syntax
+- The url to grab the form output from
+```ts
+parseURLParams(url: string): void;
+```
+
+### returnConsole();
+Simply sends the function that is ran to the console. (may get removed, as it is redundant).
+#### Syntax
+- The function to grab the result from.
+```ts
+returnConsole(event: string): void;
+```
+
+### MinMaxValue();
+If the value is not within the minium and maxium number it will send an error via console.
+#### Syntax
+- value The number to check if it is between the min and max.
+- min The minium allowed number.
+- max The maxium allowed number.
+```ts
+MinMaxValue(value: number, min: number, max: number): void;
+```
+
+### errorClass();
+It will add the class `error` to the element, You can then use CSS to add custom formatting to the element that has the error.
+#### Syntax
+- element The querySelector to use.
+- action The action to use. `add` or `remove`
+```ts
+errorClass(element: string, action: enum ): void;
+```
+
+### disableElement();
+Disables the element. (for input element).
+#### Syntax
+- element The querySelector to use.
+```ts
+disableElement(selector: string): void;
+```
+
+### enableElement();
+Enables the element. (for input element).
+#### Syntax
+- element The querySelector to use.
+```ts
+enableElement(selector: string): void;
+```
+
+### openURL();
+Opens the url in a new tab.
+#### Syntax
+- element The querySelector to use.
+```ts
+openURL(selector: string): void;
+```
+
+### hideElement();
+Hides the element.
+#### Syntax
+- element The querySelector to use.
+```ts
+hideElement(selector: string): void;
+```
+
+### showElement();
+Shows the element that is hidden.
+#### Syntax
+- element The querySelector to use.
+```ts
+showElement(selector: string): void;
+```
+
+### sendError();
+Sends an error to console and to alert.
+#### Syntax
+- errorMessage The message to use for the error.
+```ts
+sendError(errorMessage: string): void;
+```
+
+
+# modalWindow
+This contains a function that allows you to easily create a custom modal window. read more here https://www.w3schools.com/howto/howto_css_modals.asp. Note that this function does use some Jquery functions. More features are planned.
+## Install
+Install [modalWindow.js]()
+```html
+<!--Jquery-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<!--Modal Window-->
+<link rel="stylesheet" type="text/css" href="" />
+<script src=""></script>
+```
+
+## Functions
+### modalWindow();
+#### Javascript Syntax
+- modalID The id of the modal
+- type How to interprit the input. Only excepts 'json'
+- input The json to use
+```ts
+modalWindow(modalID: string, type: enum, input: object): void;
+```
+#### JSON Syntax
+```
+theme: {dark, light} 
+title:
+    text: {String}
+    header: {Integer [1-6]} 
+    color: {String}
+    font: {String}
+    bold: {Boolean} default: false
+    italic: {Boolean} default: false
+    underlined: {Boolean} default: false
+    strikethrough: {Boolean} default: false
+    align: {String} default: false
+
+body: {Array}
+    text: {String}
+    header: {Integer [1-6]} 
+    color: {String}
+    font: {String}
+    bold: {Boolean} default: false
+    italic: {Boolean} default: false
+    underlined: {Boolean} default: false
+    strikethrough: {Boolean} default: false
+    align: {String} default: false
+    style: {Enum [button]}
+    embed: {String}
+    input:
+        id: {String} default: random number
+        type: {String}
+        examples: {String}
+
+    codeblock:
+        id: {String}
+        width: {Integer|string}
+        height: {Integer|string}
+        readonly: {Boolean}
+        extension: {String} when 'text/json' it will use JSON
+        code:
+            type: {contents, ref, selector}
+            pretty_print: {Boolean}
+            value: {String}
+
+    <--Experimetal-->
+    clickEvent:
+        action: {open_url, copy_to_clipboard, run_function}
+        value: {String}
+    changeEvent:
+        action: {open_url, copy_to_clipboard, run_function}
+        value: {String}
+    hoverEvent:
+        action: {open_url, copy_to_clipboard, run_function}
+        value: {String}
+
+```
+#### Example
+```js
+var options = {
+    'theme': 'dark',
+    'title': {
+        'text': 'View JSON',
+        'header': 1
+    },
+    'body': [
+        {
+            'text': 'View the discord message.'
+        },
+        {
+            'text': ' ',
+            'codeblock': {
+                'width': '100%',
+                'height': '50%',
+                'readonly': true,
+                'extension': 'text/json',
+                'code': {
+                    'type': 'contents',
+                    'pretty_print': true,
+                    'value': {
+                        "content": "This is the message that shows up in discord!",
+                        "embeds": [
+                            {
+                                "title": "Embed title",
+                                "description": "This is a description for embed.",
+                                "author": "Your Mom",
+                                "fields": [
+                                    {
+                                        "name": "What is a field?",
+                                        "value": "IDK?"
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+    ]
+}
+modalWindow('viewjson','json',options)
+```
+**Result:**
+
+![Not Added]()
+
+### closeModal();
+Used to close the current modal window, used inside modalWindow function
+#### Syntax
+```ts
+closeModal(): void;
+```
---
+++ libs/modalWindow/1.0.0/script.js
@@ -0,0 +1,243 @@
+$(document).ready(function () {
+    console.log('loaded modalWindow.js')
+});
+
+/**
+ * Creates a new window to add diffrent functions! Make sure to have the default modal CSS added.
+ * @example
+ * // Will create a modal window (with id test)
+ * var contents = {theme: 'dark', title: {text:'Window Title'}, body: [{text: 'Sone text...'}]}
+ * modalWindow('example-modal', 'json', contents)
+ * @param {string} modalID The id of the modal
+ * @param {string} type The type of input to read, only supports JSON
+ * @param {string} input The JSON data to create the window
+*/
+function modalWindow(modalID, type, input) {
+    if (input.width != undefined) { var Width = 'width: ' + input.width + ';' } else { var Width = '' }
+    if (input.height != undefined) { var Height = 'height: ' + input.height + ';' } else { var Height = '' }
+    var base = '<div id="' + modalID + '-container" class="modal"><div class="modal-content" style="' + Width + Height + '"><span class="close">&times;</span><div id="' + modalID + '"></div></div></div>'
+    var createModal = document.createElement("div");
+    createModal.innerHTML = base;
+    document.body.appendChild(createModal);
+
+    // build
+    if (type == 'json') {
+        var file = $('#' + modalID)
+        // Create Contents
+        if (input.theme == 'dark') {
+            document.querySelector('.modal-content').classList.add('dark')
+        } else {
+            document.querySelector('.modal-content').classList.add('light')
+        }
+        if (input.title) {
+            file.append(styleBuilder(input.title, input.title.text))
+        };
+        if (input.body) {
+            input.body.forEach(
+                input => {
+                    if (input.text) {
+                        file.append(styleBuilder(input, input.text))
+                    }
+                    if (input.translate) {
+                        // .replace(/(^%s)/g, '<span id="replace' + i + '"></span>')
+                        var output = input.translate.replace(/(%%)([1-9])/g, '<span id="replace$2"></span>')
+                        if (output.includes('%s')) { sendError('Replacement "%s" has not been added yet! please use "%%[1-9]" instead.') }
+                        file.append(styleBuilder(input, output))
+                        for (let i = 1; i < input.with.length + 1; i++) {
+                            var id = 'replace' + i
+                            document.getElementById(id).innerHTML = input.with[i - 1]
+                        }
+                    }
+                }
+            )
+        }
+    } else {
+        sendError('Unknown type for function ModalWindow()')
+    }
+    function styleBuilder(json, text) {
+        var Color = ''
+        var Font = ''
+        var Bold = ''
+        var Italic = ''
+        var Underline = ''
+        var Strikethrough = ''
+        /* Common */
+        if (json.color != undefined) { var Color = ' color:' + json.color + ';' } else { var Color = '' };
+        if (json.font != undefined) { var Font = ' font-family:' + json.font + ';' } else { var Font = '' };
+        if (json.bold != undefined) { if (json.bold == true) { var Bold = ' font-weight: bold;' } else { var Bold = '' } } else { var Bold = '' };
+        if (json.italic != undefined) { if (json.italic == true) { var Italic = ' font-style: italic;' } else { var Italic = '' } } else { var Italic = '' };
+        if (json.underlined != undefined) { if (json.underlined == true) { var Underline = 'underline ' } else { var Underline = '' } } else { var Underline = '' };
+        if (json.strikethrough != undefined) { if (json.strikethrough == true) { var Strikethrough = 'line-through ' } else { var Strikethrough = '' } } else { var Strikethrough = '' };
+
+        if (json.align != undefined) { var Align = 'text-align:' + json.align + ';' } else { var Align = '' };
+        if (json.header != undefined) { if (json.header <= 6) { var element = 'h' + json.header } else { var element = 'p'; sendError('max level for header is 6, but got ' + json.header) } } else { var element = 'p' };
+        var decor = ' text-decoration:' + Underline + Strikethrough + ';'
+        var style = Color + Bold + Italic + Font + Align + decor
+
+        /* clickEvent */
+        if (json.clickEvent != undefined) {
+            if (json.clickEvent.action == 'open_url') {
+                console.log(json.clickEvent.value)
+                //var click = ' onclick="window.open(' + json.clickEvent.value + ')"'
+                var click = ''
+            } else {
+                if (json.clickEvent.action == 'copy_to_clipboard') {
+                    console.log(json.clickEvent.value)
+                    var click = ' copyElement(' + json.clickEvent.value + ')"'
+                } else {
+                    if (json.clickEvent.action == 'run_function') {
+                        console.log(json.clickEvent.value)
+                        var click = json.clickEvent.value
+                    } else {
+                        sendError('Unknown action type, expected "open_url", "copy_to_clipboard" or "run_function" but got "' + json.clickEvent.action + '"')
+                    }
+                }
+            }
+            var click = ''
+        }
+        /* changeEvent */
+        if (json.changeEvent != undefined) {
+            if (json.changeEvent.action == 'open_url') {
+                console.log(json.changeEvent.value)
+                var click = ''
+            } else {
+                if (json.changeEvent.action == 'copy_to_clipboard') {
+                    console.log(json.changeEvent.value)
+                    var click = ' copyElement(' + json.changeEvent.value + ')"'
+                } else {
+                    if (json.changeEvent.action == 'run_function') {
+                        console.log('onchange=' + json.changeEvent.value)
+                        var click = ' onchange="' + json.changeEvent.value + '"'
+                    } else {
+                        sendError('Unknown action type, expected "open_url", "copy_to_clipboard" or "run_function" but got "' + json.changeEvent.action + '"')
+                    }
+                }
+            }
+            var click = ''
+        }
+        /* hoverEvent */
+        if (json.hoverEvent != undefined) {
+            if (json.hoverEvent.action == 'show_text') {
+                var title = ' title="' + json.hoverEvent.contents.text + '" '
+            } else {
+                var title = ''
+            }
+        } else {
+            var title = ''
+        }
+        /* embed */
+        if (json.embed != undefined) {
+            console.log(json.embed)
+            var text = json.embed
+        }
+        /* input */
+        if (json.input != undefined) {
+            /* UUID */
+            if (json.input.id == undefined) { var UUID = rng(1111, 9999) } else { var UUID = json.input.id };
+            if (json.input.type != undefined) { var Type = json.input.type } else { var Type = 'text' }
+            if (json.input.examples != undefined) {
+                var List = ' list="' + UUID + '-list"'
+                json.input.examples.forEach(json => {
+                    /* create datalist */
+                    var createModal = document.createElement("datalist");
+                    createModal.id = UUID + '-list'
+                    document.body.appendChild(createModal);
+
+                    console.log(document.getElementById(UUID + '-list'))
+                    $('#' + UUID + '-list').append('<option value="' + json + '">' + json + '</option>')
+                })
+            } else {
+                var List = ''
+            }
+            var input = '<input class="modal-input' + List + ' id="' + UUID + '" type="' + Type + '"/>'
+        }
+        /* Code block */
+        if (json.codeblock != undefined) {
+            /* ID */
+            var codeFile = json.codeblock.code
+            if (json.codeblock.id == undefined) { var ID = rng(1111, 9999) } else { var ID = json.codeblock.id };
+            if (json.codeblock.width != undefined) { var Width = ' width:' + json.codeblock.width + ';' } else { var Width = '' };
+            if (json.codeblock.height != undefined) { var Height = ' height:' + json.codeblock.height + ';' } else { var Height = '' };
+            if (json.codeblock.readonly == true) { var Readonly = 'readonly' } else { var Readonly = '' };
+            if (json.codeblock.extension == 'text/json') {
+                /* contents use the JSON inside 'values'*/
+                if (codeFile.type == 'contents') {
+                    if (codeFile.pretty_print == true) {
+                        var code = JSON.stringify(code.value, null, 4)
+                    } else {
+                        var code = JSON.stringify(code.value)
+                    }
+                }
+                /* referance to URL */
+                if (codeFile.type == 'ref') {
+                    var code = ''
+                    $.getJSON(codeFile.value, function (value) {
+                        if (codeFile.pretty_print == true) {
+                            $('#' + ID).append(JSON.stringify(value, null, 4))
+                        } else {
+                            $('#' + ID).append(JSON.stringify(value))
+                        }
+                    })
+                }
+                /* Grabs the contents from a diffrent element */
+                if (codeFile.type == 'selector') {
+                    if (codeFile.pretty_print == true) {
+                        var code = JSON.stringify(document.querySelector(codeFile.value).innerHTML, null, 4)
+                    } else {
+                        var code = JSON.stringify(document.querySelector(codeFile.value).innerHTML)
+                    }
+                }
+            } else {
+                /* contents use the JSON inside 'values'*/
+                if (codeFile.type == 'contents') {
+                    var code = codeFile.value
+                }
+                /* Grabs the contents from a diffrent element */
+                if (codeFile.type == 'selector') {
+                    var code = document.querySelector(codeFile.value).innerHTML
+                }
+            }
+            var input = '<br><textarea class="modal-codeblock" id="' + ID + '" onchange="updateText(' + ID + ')"' + click + ' style="' + Width + Height + '" ' + Readonly + '>' + code + '</textarea>'
+        }
+        /* Button */
+        if (json.style == 'button') {
+            return '<button class="modal-button"' + click + ' style="' + style + '">' + text + '</button>'
+        } else {
+            if (json.input != undefined) {
+                return '<label for="' + UUID + '" ' + click + title + 'style="' + style + '">' + text + ' </label>' + input
+            } else {
+                if (json.codeblock != undefined) {
+                    return '<label for="' + ID + '" ' + title + 'style="' + style + '">' + text + ' </label>' + input
+                } else {
+                    return '<' + element + ' ' + click + title + 'style="' + style + '">' + text + '</' + element + '>'
+                }
+            }
+        }
+    }
+
+
+    var modal = document.querySelector('#' + modalID + '-container')
+    document.querySelector('#' + modalID + '-container').style.display = "block";
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function () {
+        //modal.style.display = "none";
+        closeModal()
+    }
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            //modal.style.display = "none";
+            closeModal()
+        }
+    }
+}
+
+/**
+ * Closes/removes the modal window
+ * @example
+ * // Will close the class 'modal'
+ * closeModal()
+*/
+function closeModal() {
+    var removeElement = document.querySelector('.modal');
+    removeElement.remove();
+}
