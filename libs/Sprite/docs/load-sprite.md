7b09f83e89278c3f474e2ee3116778433355460b
Legopitstop <1589lego@gmail.com>
2021-10-22 10:42:52 -0500

    loadSprite will new need a sprite JSON

--- libs/Sprite/docs/load-sprite.md
+++ libs/Sprite/docs/load-sprite.md
@@ -7,33 +7,33 @@ Will place a sprite image from a sprite sheet using a JSON that contains all the
 ---
 ## Syntax
 ```typescript
-loadSprite(id: string, selector: string): void
+loadSprite(id: String, selector: String, s: JSON): String | false
 ```
-- `id` - The id of the texture that you want to render.
-- `selector` - The query selector to append the sprite to.
+- `id` The id to search for.
+- `selector` The query selector to apply the sprite to.
+- `s` The spritesheet JSON to search for the id in.
 
 [Back to top](#)
 ## Example
 ### HTML
 ```html
-<link href="./sprites.json" rel="spritesheet">
+<script src="https://cdn.jsdelivr.net/gh/legopitstop/Javascript/libs/Sprite/sprite.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/legopitstop/Javascript/libs/Sprite/sprite.css">
+<div id="sprites"></div>
 ```
-### JSON
-```json
-{
+### JAVASCRIPT
+This will append inside the element with the id `sprites`
+```js
+var spritesheet = {
     "$schema": "https://cdn.jsdelivr.net/gh/legopitstop/Javascript/libs/Sprite/schema.json",
     "file": "./assets/spritesheet.png",
     "width": 32,
     "height": 32,
     "textures": {
-        "sprite": [1,1],
-        "sprite2": [2,1]
+        "sprite": [1, 1],
+        "sprite2": [2, 1]
     }
 }
-```
-### JAVASCRIPT
-This will append inside the element with the id `sprites`
-```js
-loadSprite("sprite", "#sprites")
+loadSprite("sprite", "#sprites", spritesheet)
 ```
 [Back to top](#)
--- libs/Sprite/sprite.js
+++ libs/Sprite/sprite.js
@@ -1,20 +1,36 @@
-function loadSprite(id, selector) {
-    if (document.querySelectorAll('link[rel=spritesheet]')) {
-        var spritesheet = document.querySelectorAll('link[rel=spriteheet]')
-        for (let i = 0; i < spritesheet.length; i++) {
-            $.getJSON(spritesheet[i].href, function (s) {
+/**
+ * 
+ * @param {String} id The id to search for
+ * @param {String} selector The query selector to apply the sprite to
+ * @param {JSON} s The spritesheet JSON to search for the id in.
+ * @returns If successful it will return the id of the sprite
+ */
+ function loadSprite(id, selector, s) {
+    if (id) {
+        if (selector) {
+            if (s) {
                 if (s.file) {
                     if (s.textures[id]) {
+                        var eid = id.replace(/:/gm, '-') + '-' + randomId()
                         var u = -s.textures[id][0] * s.width + s.width
                         var v = -s.textures[id][1] * s.height + s.height
-                        $(selector).append('<span class="invslot-item"><span class="sprite" style="background-image: url(' + s.file + ');background-position: ' + u + 'px ' + v + 'px;height: ' + s.height + 'px;width: ' + s.width + 'px;"></span></span>');
-                        return true
+                        $(selector).append(`<span id="${eid}" class="invslot-item"><span class="sprite" style="background-image: url('${s.file}');background-position:${u}px ${v}px; height:${s.height}px; width:${s.width}px;"></span></span>`);
+                        return eid
                     }
                 } else {
-                    console.error('missing file')
-                    return false
+                    console.error('missing file');
+                    return false;
                 }
-            });
+            } else {
+                console.error('Missing varable "s"');
+                return false;
+            }
+        } else {
+            console.error('Missing varable "selector"');
+            return false;
         }
+    } else {
+        console.error('Missing varable "id"');
+        return false;
     }
 }
